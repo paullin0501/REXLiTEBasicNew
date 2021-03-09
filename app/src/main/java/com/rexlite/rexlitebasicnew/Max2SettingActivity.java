0e14e32bcecb67c6bf684cd429f0b07cdd783fc5
paul <paullin050182@gmail.com>
2021-03-10 00:10:37 +0800

    03/10 expand cardview commit

--- app/src/main/java/com/rexlite/rexlitebasicnew/Max2SettingActivity.java
+++ app/src/main/java/com/rexlite/rexlitebasicnew/Max2SettingActivity.java
@@ -1,8 +1,13 @@
 package com.rexlite.rexlitebasicnew;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.transition.TransitionManager;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -24,7 +29,11 @@ public class Max2SettingActivity extends AppCompatActivity  {
         SeekBar seekBar = findViewById(R.id.seekBar);
         final Button ch1Button = (Button) findViewById(R.id.ch1_button);
         final Button ch2Button = (Button) findViewById(R.id.ch2_button);
-        //TimePicker設定
+        //擴充功能
+        final Button hiddenButton = findViewById(R.id.expand_button);
+        final CardView cardView = findViewById(R.id.cardview);
+        final ConstraintLayout hiddenView = findViewById(R.id.hidden_view);
+        ;        //TimePicker設定
         final TimePicker timePicker = findViewById(R.id.timepicker);
         final TextView timeText = findViewById(R.id.setting_timing);
         timePicker.setIs24HourView(true);
@@ -84,7 +93,36 @@ public class Max2SettingActivity extends AppCompatActivity  {
             }
         });
 
+    //卡片擴充按鍵
+        hiddenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                // If the CardView is already expanded, set its visibility
+                //  to gone and change the expand less icon to expand more.
+                if (hiddenView.getVisibility() == View.VISIBLE) {
+
+                    // The transition of the hiddenView is carried out
+                    //  by the TransitionManager class.
+                    // Here we use an object of the AutoTransition
+                    // Class to create a default transition.
+                    TransitionManager.beginDelayedTransition(cardView,
+                            new AutoTransition());
+                    hiddenView.setVisibility(View.GONE);
+                    hiddenButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_expand_more_24,0,0,0);
+                }
+
+                // If the CardView is not expanded, set its visibility
+                // to visible and change the expand more icon to expand less.
+                else {
+
+                    TransitionManager.beginDelayedTransition(cardView,
+                            new AutoTransition());
+                    hiddenView.setVisibility(View.VISIBLE);
+                    hiddenButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_expand_less_24,0,0,0);
+                }
+            }
+        });
 
     }
   /*  public void ch1ButtonSelected(View view){
--- app/src/main/java/com/rexlite/rexlitebasicnew/RelayFragment.java
+++ app/src/main/java/com/rexlite/rexlitebasicnew/RelayFragment.java
@@ -1,9 +1,9 @@
 package com.rexlite.rexlitebasicnew;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +12,17 @@ import android.widget.SeekBar;
 
 public class RelayFragment extends Fragment {
 
-    boolean liteStatus =false; //判斷目前燈是開或關的狀態
+    boolean lightStatus =false; //判斷目前燈是開或關的狀態
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_relay, container, false);
+
+        return inflater.inflate(R.layout.fragment_relay, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         SeekBar seekBar =view.findViewById(R.id.seekBar);
         //seekbar設定
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -34,21 +40,19 @@ public class RelayFragment extends Fragment {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 int progress = seekBar.getProgress();
 
-                if(!liteStatus & progress > 0) {
+                if(!lightStatus & progress > 0) {
                     seekBar.setProgress(100);
-                    liteStatus = true;
+                    lightStatus = true;
                 }
-                else if(liteStatus & progress < 100) {
+                else if(lightStatus & progress < 100) {
                     seekBar.setProgress(0);
-                    liteStatus = false;
+                    lightStatus = false;
                 }
             }
         });
-        // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_relay, container, false);
     }
-   /* @Override
+
+    /* @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
